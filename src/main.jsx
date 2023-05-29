943701032aec9eef9de8168fca08fc51213fc865
lucas <lucasfrutig0@gmal.com>
2023-05-29 12:22:35 -0300

    feature: add vans page to render a list of vans

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+import "./server";
+import Vans from "./pages/Vans";
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,11 +16,13 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
